9f95e5ad66175fe22b19eaf5e778c70155b90d75
Fellipy Wagmacker <126357141+FellipyWagmacker2@users.noreply.github.com>
2023-05-15 12:51:43 -0300

    Delete ProgWeb/SemResponsividade directory

--- ProgWeb/SemResponsividade/js/script_sobre.js
+++
@@ -1,6 +0,0 @@
-import {send} from "./script_base.js";
-
-// Enviar email do footer
-document.querySelector("#enviarEmail").addEventListener("click", () =>{
-    send(document.querySelector("#email"))
-});
