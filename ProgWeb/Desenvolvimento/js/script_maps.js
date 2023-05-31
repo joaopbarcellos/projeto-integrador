11ab8416f30439611678883f66029942671d01c0
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-05-31 12:41:05 -0300

    Add files via upload

--- ProgWeb/Desenvolvimento/js/script_maps.js
+++ ProgWeb/Desenvolvimento/js/script_maps.js
@@ -8,11 +8,33 @@ function calculaTamanhoMapa(mapa){
 
 function loadMapScenario() {
     const mapa = document.getElementById("mapa");
+    const locIfes = Microsoft.Maps.Location(-20.197329691804068, -40.2170160437478);
     calculaTamanhoMapa(mapa)
-    var map = new Microsoft.Maps.Map(document.getElementById("mapa"), {
-        center: new Microsoft.Maps.Location(-20.197396103583603, -40.217106774152555), 
-        mapTypeId: Microsoft.Maps.MapTypeId.aerial, zoom:100
-    });
-    var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-20.197396103583603, -40.217106774152555), {color: "green", title: "Sede Time In"})
-    map.entities.push(pushpin);
+    
+    if(window.innerWidth <= 540){
+        maps = new Microsoft.Maps.Map(document.getElementById("mapa"), {
+            center: locIfes,
+            zoom: 100,
+            NavigationBarMode: "minified",
+            navigationBarOrientation: "vertical",
+            showMapTypeSelector: false,
+            showLocateMeButton: true,
+        });
+    }
+    else{
+         maps = new Microsoft.Maps.Map(document.getElementById("mapa"), {
+            center: locIfes,
+            zoom: 100,
+            NavigationBarMode: "minified",
+            navigationBarOrientation: "horizontal",
+            showLocateMeButton: true
+        });
+    }
+
+    // var pushpin = new Microsoft.Maps.Pushpin(new locIfes, {color: "green", title: "Sede Time In"})
+    // mapa.entities.push(pushpin);
 }
+
+window.addEventListener("resize", (e) =>{
+    calculaTamanhoMapa(document.getElementById("mapa"));
+} )
