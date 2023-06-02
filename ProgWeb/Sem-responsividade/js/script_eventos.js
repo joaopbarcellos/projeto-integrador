67945904cecf4e334d997ed6c4dd217be89f9927
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-06-02 15:54:44 -0300

    Delete ProgWeb/Sem-responsividade directory

--- ProgWeb/Sem-responsividade/js/script_eventos.js
+++
@@ -1,7 +0,0 @@
-let voltar = document.querySelector(".cssbuttons-io-button");
-if(voltar){
-  // Adicionando o ouvidor do evento para se inscrever
-  voltar.addEventListener("click", () => {
-    window.location.assign("../home.html")
-})
-}
