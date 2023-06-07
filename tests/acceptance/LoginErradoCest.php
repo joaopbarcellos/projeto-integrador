22d718f71040364762d59b9716ec030b1a4c8f6f
Gustavo Alves Caetano <91470894+GustavoACaetano@users.noreply.github.com>
2023-06-07 08:24:56 -0300

    Create LoginErradoCest.php

---
+++ tests/acceptance/LoginErradoCest.php
@@ -0,0 +1,24 @@
+
+
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class LoginErradoCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function loginErrado(AcceptanceTester $I)
+    {
+      $I->amOnPage('/projeto-integrador/ProgWeb/Desenvolvimento/index.html');
+      $I->fillField('null', 'dadwadwadadwadwa');
+      $I->click('Entrar');
+      $I->see('E-mail está fora dos padrões!')
+    }
+}
