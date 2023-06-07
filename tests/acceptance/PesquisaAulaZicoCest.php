2c23253ccecca0897c8e17d71c0818bbaed2e8e7
Gustavo Alves Caetano <91470894+GustavoACaetano@users.noreply.github.com>
2023-06-07 08:20:23 -0300

    Create PesquisaAulaZicoCest.php

---
+++ tests/acceptance/PesquisaAulaZicoCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class PesquisaAulaZico
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function PesquisaAulaZico(AcceptanceTester $I)
+    {
+	    $I = new AcceptanceTester($scenario); 
+      $I->amOnPage('/projeto-integrador/ProgWeb/Desenvolvimento/home.html'); 
+      $I->fillField('null', 'Aula do Zico'); 
+      $I->click('🔍'); 
+      $I->click('Saber Mais'); 
+      $I->seeCurrentURLEquals('/projeto-integrador/ProgWeb/Desenvolvimento/eventos/aulaZico.html');
+    }
+}
