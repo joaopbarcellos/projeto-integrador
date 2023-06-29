55572bb6cd3a9d7532e096b41b1644116bc7b42c
Gustavo Alves Caetano <91470894+GustavoACaetano@users.noreply.github.com>
2023-06-29 11:22:38 -0300

    Update LoginErradoCest.php

--- tests/acceptance/LoginErradoCest.php
+++ tests/acceptance/LoginErradoCest.php
@@ -14,7 +14,7 @@ class LoginErradoCest
     // tests
     public function loginErradoCest(AcceptanceTester $I)
     {
-        $I->amOnPage('/projeto-integrador/Desenvolvimento/index.html');
+        $I->amOnPage('/projeto-integrador/ProgWeb/Desenvolvimento/index.html');
         $I->fillField('emailCampo', 'dwadawdawdada');
         $I->click('Entrar');
         $I->see('E-mail deve ser preenchido corretamente!');
