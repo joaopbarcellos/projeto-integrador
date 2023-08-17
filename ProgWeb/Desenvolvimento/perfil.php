eb7ceb9544d1b6054005cb972c666dc5b5c1ec44
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-08-17 11:53:13 -0300

    Add files via upload

---
+++ ProgWeb/Desenvolvimento/perfil.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="logos/icon.png">
+	<title>Time In</title>
+	<link rel="stylesheet" href="css/styles_base.css">
+
+	<link rel="stylesheet" href="css/styles_perfil.css">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+	<script src="js/script_perfil.js" type="module" defer></script>
+
+	<script src="js/script_base.js" type="module" defer></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.5/dist/sweetalert2.all.min.js"></script>
+
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"
+		defer></script>
+
+</head>
+
+<body>
+	<header>
+		<?php include('header.php') ?>
+	</header> <!-- Fechando header -->
+
+	<label class="switch">
+		<span class="sun"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<g fill="#ffd43b">
+					<circle r="5" cy="12" cx="12"></circle>
+					<path
+						d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z">
+					</path>
+				</g>
+			</svg></span>
+		<span class="moon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+				<path
+					d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z">
+				</path>
+			</svg></span>
+		<input type="checkbox" class="input">
+		<span class="slider"></span>
+	</label>
+
+	<div id="container">
+		<div class="col-10 col-md-6 col-lg-5 col-xg-3 col-xxl-4" id="alinhar">
+			<!-- Crio a section com a carteirinha -->
+			<section id="perfil">
+
+				<!-- Crio a div com as informacoes do ususario -->
+				<div class="fot_desc">
+					<div class="row col-12">
+
+						<div class="col-12 col-lg-4 text-center">
+							<!-- Como padrao o usuario n possui foto -->
+							<img src="img/semfoto.png" id="semfoto"></img>
+						</div>
+						<div class="col-12 col-lg-8 ">
+							<!-- Crio a div pra inserir os dados -->
+							<div class="dados">
+
+								<!-- Crio a div do nome -->
+								<div class="info">
+
+									<label class="labels texto word-break"> Nome: </label>
+
+									<!-- Escrevo na label o nome do usuario -->
+									<label class="cadastro texto word-break" id="nome_usuario"> </label>
+
+									<!-- Fecho a div do nome -->
+								</div>
+
+								<!-- Crio a div do nascimento-->
+								<div class="info">
+
+									<label class="labels texto word-break">Data de Nascimento: </label>
+
+									<!-- Escrevo na label o nascimento do usuario -->
+									<label class="cadastro texto word-break" id="dataNasc_usuario"> </label>
+
+									<!-- Fecho a div do nascimento -->
+								</div>
+
+								<!-- Crio a div de email-->
+								<div class="info" id="email_user">
+
+									<label class="labels texto word-break">Email: </label>
+
+									<!-- Escrevo na label o email do usuario -->
+									<label class="cadastro texto word-break" id="email_usuario"> </label>
+
+									<!-- Fecho a div de email -->
+								</div>
+							</div>
+							<!-- Fecho a div pra inserir os dados -->
+						</div>
+					</div>
+				</div>
+
+				<!-- Crio o rodape da carteirinha com logo e hashtag -->
+				<div id="hashtag">
+
+					<p class=" word-break" id="sloganperfil">#AlwaysInMovement</p>
+
+					<img src="logos/icon2.png" id="logo_cart"></img>
+
+				</div>
+
+			<!-- Fecho a section com a carteirinha -->
+			</section>
+			
+			<div id="botoes"> 
+				<button id="editar"><svg class="iconeeditar" xmlns="http://www.w3.org/2000/svg" height="1em"
+						viewBox="0 0 512 512">
+						<path
+							d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+					</svg>
+					Editar Informações</button> <!-- crio o botão de editar insformações -->
+				<button id="excluir"><svg class="iconeexcluir" xmlns="http://www.w3.org/2000/svg" height="1em"
+						viewBox="0 0 448 512">
+						<path
+							d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+					</svg>
+					Excluir Perfil</button> <!-- crio o botão de excluir perfil -->
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<?php include('footer.php') ?>
+	</footer>
+
+</body>
+
+</html>
---
+++ ProgWeb/Desenvolvimento/sobre.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="logos/icon.png">
+	<title>Time In</title>
+	<link rel="stylesheet" href="css/styles_base.css">
+
+	<link rel="stylesheet" href="css/styles_sobre.css">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+	<script src="js/script_base.js" type="module" defer></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.5/dist/sweetalert2.all.min.js"></script>
+
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"
+		defer></script>
+
+</head>
+<body>
+	<!-- Header com a navbar -->
+	<header>
+		<?php include('header.php') ?>
+	</header> <!-- Fechando header -->
+
+	<!-- Div que fala um pouco sobre a equipe -->
+
+	<div class="m-auto col-11 col-md-10" id="sobre">
+
+		<!--- Título da div -->
+		<h1 class="fs-1 text">Sobre nós</h1>
+
+		<p class="fs-sm-6 fs-lg-5">
+			Somos uma equipe dedicada e apaixonada por esportes, e criamos esta plataforma para ajudar os organizadores
+			de eventos e os participantes
+			a se conectarem de forma fácil e eficiente. Nosso objetivo é tornar a participação em eventos esportivos o
+			mais fácil e acessível possível.
+			Para isso, oferecemos uma variedade de ferramentas que ajudam a organizar e promover eventos, além de
+			permitir que os participantes
+			encontrem e se inscrevam para eventos em todo o país. Nós trabalhamos com organizadores de eventos
+			esportivos de diferentes modalidades, desde
+			corridas de rua até competições de ciclismo, para garantir que nossa plataforma atenda às necessidades de
+			todos os tipos de eventos.
+			Nós nos esforçamos para oferecer uma experiência online sem complicações para todos os nossos usuários.
+			Através de nossa interface intuitiva,
+			os organizadores de eventos podem criar e gerenciar seus eventos com facilidade, enquanto os participantes
+			podem encontrar e se inscrever para
+			eventos com apenas alguns cliques. Além disso, nossa equipe de suporte está sempre pronta para ajudar em
+			caso de quaisquer problemas ou dúvidas.
+			Queremos garantir que todos os nossos usuários tenham uma experiência positiva em nosso site. Obrigado por
+			escolher a Time In como sua plataforma
+			de eventos esportivos preferida. Estamos ansiosos para ajudá-lo a descobrir novos eventos emocionantes e
+			apoiá-lo em sua jornada esportiva.
+
+		</p>
+	</div>
+
+	<!--- DIV COM UM VIDEO SOBRE A GENTE -->
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="video-container">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/EHcIMmAS14Y"
+						frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<h2 class="h2_equipe">Equipe</h2>
+
+	<div class="col-11 m-auto p-auto">
+		<div class="row">
+
+			<div class="participante col-12 col-sm-6 col-md-4 col-lg-3">
+				<!-- Div Fellipy -->
+				<div class="pessoas">
+
+					<!-- h3 com nome do integrante -->
+					<h3 class="word-break">Fellipy Silva Pereira Wagmackerㅤㅤㅤㅤ</h3>
+
+					<!-- Foto do integrante -->
+					<img class="foto" src="img/pi.png">
+
+					<!-- Span que inclui a imagem do instagram e o link puxando para o instagram de cada integrante  -->
+					<span class="span_integrantes">
+
+						<svg class="iconeSobre" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+
+
+						<a href="https://www.instagram.com/fellipy_wagmacker/" target="_blank"
+							id="textos">fellipy_wagmacker</a>
+
+					</span>
+
+					<!-- Fim da div pessoas div_fellipy -->
+				</div>
+			</div>
+
+			<!-- col-xl-5 col-xxl-3 -->
+			<div class="participante col-12 col-sm-6 col-md-4 col-lg-3">
+				<!-- Div gustavo -->
+				<div class="pessoas">
+
+					<!-- h3 com nome do integrante -->
+					<h3 class="word-break">Gustavo Alves Caetanoㅤㅤㅤㅤㅤㅤㅤㅤ</h3>
+
+					<!-- Foto do integrante -->
+					<img class="foto" src="img/gu.png">
+
+					<!-- Span que inclui a imagem do instagram e o link do instagram de cada integrante  -->
+					<span class="span_integrantes">
+
+						<svg class="iconeSobre" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+
+
+						<a href="https://www.instagram.com/gustavo.alcaetano/" target="_blank"
+							id="textos">gustavo.alcaetano</a>
+
+					</span>
+
+
+
+					<!-- Fim da div pessoas div_gustavo -->
+				</div>
+			</div>
+
+			<div class="participante col-12 col-sm-6 col-md-4 col-lg-3">
+				<!-- Div Barcellos -->
+				<div class="pessoas">
+
+					<!-- h3 com nome do integrante -->
+					<h3 class="word-break">João Pedro Zamborlini Barcellosㅤㅤㅤㅤ</h3>
+
+					<!-- Foto do integrante -->
+					<img class="foto" src="img/jp.png">
+
+					<!-- Span que inclui a imagem do instagram e o link do instagram de cada integrante -->
+					<span class="span_integrantes">
+
+					<svg class="iconeSobre" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+
+
+						<a href="https://www.instagram.com/jp_barc/" target="_blank" id="textos">jp_barc</a>
+
+					</span>
+
+
+					<!-- Fim da div pessoas div_Barcellos -->
+				</div>
+			</div>
+
+			<div class="participante col-12 col-sm-6 col-md-4 col-lg-3 m-auto">
+				<!-- Div Pedro -->
+				<div class="pessoas">
+
+					<!-- h3 com nome do integrante -->
+					<h3 class="word-break">Pedro Ramos Leite Soaresㅤㅤㅤㅤㅤㅤㅤ</h3>
+
+					<!-- Foto do integrante -->
+					<img class="foto" src="img/pe.png">
+
+					<!-- Span que inclui a imagem do instagram e o link do instagram de cada integrante -->
+					<span class="span_integrantes">
+
+						<svg class="iconeSobre" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+
+						<a href="https://www.instagram.com/pedrorls_/" target="_blank" id="textos">pedrorls_</a>
+
+					</span>
+
+					<!-- Fim da div pessoas div_Pedro -->
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+	<footer>
+		<?php include('footer.php') ?>
+	</footer>
+</body>
+
+</html>
