376f139b28ad6f0637a3d92fbf363806a9a06510
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2023-10-27 12:46:48 -0300

    Update pegar_eventos.php

--- ConexaoBDMobile/pegar_eventos.php
+++ ConexaoBDMobile/pegar_eventos.php
@@ -45,54 +45,11 @@ if (isset($_GET['limit']) && isset($_GET['offset'])) {
 				$linha_foto_evento = $consulta_foto_evento->fetch(PDO::FETCH_ASSOC);
 				$evento["foto"] = $linha_foto_evento["foto"];
 
-
-				$consulta_evento_classificacao = $db_con->prepare("SELECT fk_classificacao_id FROM evento_classificacao WHERE fk_evento_id = " . $evento["id"]);
-				$consulta_evento_classificacao->execute();
-				$linha_evento_classificacao = $consulta_evento_classificacao->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_classificacao = $db_con->prepare("SELECT nome FROM classificacao WHERE id = " . $linha_evento_classificacao["fk_classificacao_id"]);
-				$consulta_classificacao->execute();
-				$linha_classificacao = $consulta_classificacao->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_intuito = $db_con->prepare("SELECT nome FROM intuito WHERE id = " . $linha["fk_intuito_id"]);
-				$consulta_intuito->execute();
-				$linha_intuito = $consulta_intuito->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_idade_publico = $db_con->prepare("SELECT intervalo FROM idade_publico WHERE id = " . $linha["fk_idade_publico_id"]);
-				$consulta_idade_publico->execute();
-				$linha_idade_publico = $consulta_idade_publico->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_endereco = $db_con->prepare("SELECT * FROM endereco WHERE id = " . $linha["fk_endereco_id"]);
-				$consulta_endereco->execute();
-				$linha_endereco = $consulta_endereco->fetch(PDO::FETCH_ASSOC);
-				
-				$consulta_tipo_logradouro = $db_con->prepare("SELECT tipo FROM tipo_logradouro WHERE id = ". $linha_endereco["fk_tipo_logradouro_id"]);
-				$consulta_tipo_logradouro->execute();
-				$linha_tipo_logradouro = $consulta_tipo_logradouro->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_bairro = $db_con->prepare("SELECT * FROM bairro WHERE id = " . $linha_endereco["fk_bairro_id"]);
-				$consulta_bairro->execute();
-				$linha_bairro = $consulta_bairro->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_cidade = $db_con->prepare("SELECT * FROM cidade WHERE id = " . $linha_bairro["fk_cidade_id"]);
-				$consulta_cidade->execute();
-				$linha_cidade = $consulta_cidade->fetch(PDO::FETCH_ASSOC);
-
-				$consulta_estado = $db_con->prepare("SELECT nome FROM estado WHERE id = " . $linha_cidade["fk_estado_id"]);
-				$consulta_estado->execute();
-				$linha_estado = $consulta_estado->fetch(PDO::FETCH_ASSOC);
-
-				$evento["classificacao"] = $linha_classificacao["nome"];
 				$evento["nome"] = $linha["nome"];
 				$evento["preco"] = number_format($linha["preco"], 2, ',', '');
 				$evento["data"] = $linha["data"];
-				$evento["min_pessoas"] = $linha["min_pessoas"];
-				$evento["max_pessoas"] = $linha["max_pessoas"];
 				$evento["horario_fim"] = $linha["horario_fim"];
 				$evento["horario_inicio"] = $linha["horario_inicio"];
-				$evento["intuito"] = $linha_intuito["nome"];
-				$evento["endereco"] = $linha_tipo_logradouro["tipo"] . " " . $linha_endereco["descricao"] . ", " . $linha_endereco["numero"] . " - " . $linha_bairro["nome"] . ", " . $linha_cidade["nome"] . " - " . $linha_estado["nome"] . ", " . $linha_endereco["cep"];
-				$evento["idade_publico"] = $linha_idade_publico["intervalo"];
 
 				// Adiciona o evento no array de eventos.
 				array_push($resposta["eventos"], $evento);
