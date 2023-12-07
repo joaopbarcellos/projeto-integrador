270f482527dfa18c527c30c1b079023c79a0953a
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-12-07 11:53:45 -0300

    Update inscrito.php

--- conexaoBancoDados/inscrito.php
+++ conexaoBancoDados/inscrito.php
@@ -3,20 +3,24 @@
 // esta inscrito no evento que ele clicou
 
 function inscrito($id_evento, $db_con){
-    $email = $_SESSION["logado"];
-
-    $consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
-    $consultaEmail->execute();
-    $linha = $consultaEmail->fetch(PDO::FETCH_ASSOC);
-    $id_logado = $linha['id'];
-
-    $consulta = $db_con->prepare("SELECT * FROM usuario_evento WHERE fk_usuario_id = '$id_logado' AND fk_evento_id='$id_evento'");
-	if($consulta->execute()){
-        $linha = $consulta->fetch(PDO::FETCH_ASSOC);
-        if ($linha){
-            return true;
-        }
-        return false;
-    }
+	if (isset($_SESSION["logado"])){
+		$email = $_SESSION["logado"];
+		
+		$consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
+		$consultaEmail->execute();
+		$linha = $consultaEmail->fetch(PDO::FETCH_ASSOC);
+		$id_logado = $linha['id'];
+		
+		$consulta = $db_con->prepare("SELECT * FROM usuario_evento WHERE fk_usuario_id = '$id_logado' AND fk_evento_id='$id_evento'");
+		if($consulta->execute()){
+		$linha = $consulta->fetch(PDO::FETCH_ASSOC);
+		if ($linha){
+		    return true;
+		}
+		return false;
+		}
+	} else {
+		return false;
+	}
 }
 ?>
