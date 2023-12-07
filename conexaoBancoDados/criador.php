3984baefea9ba1a243c81e40333b721533642e11
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-12-07 11:52:45 -0300

    Update criador.php

--- conexaoBancoDados/criador.php
+++ conexaoBancoDados/criador.php
@@ -1,23 +1,26 @@
 <?php
-session_start();
 // esse arquivo CONSULTA se o usuario
 // logado criou o evento que a pagina carregou
 
 function criador($id, $db_con){
-    $email = $_SESSION["logado"];
-
-    $consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
-    $consultaEmail->execute();
-    $linha = $consultaEmail->fetch(PDO::FETCH_ASSOC);
-    $id_logado = $linha['id'];
-
-    $consulta = $db_con->prepare("SELECT * FROM evento WHERE evento.id=$id AND evento.fk_usuario_id=$id_logado");
-	if($consulta->execute()){
-        $linha = $consulta->fetch(PDO::FETCH_ASSOC);
-        if ($linha){
-            return true;
-        }
-        return false;
+    if (isset($_SESSION["logado"])){
+	    $email = $_SESSION["logado"];
+	
+	    $consultaEmail = $db_con->prepare("SELECT * FROM usuario WHERE email='$email'");
+	    $consultaEmail->execute();
+	    $linha = $consultaEmail->fetch(PDO::FETCH_ASSOC);
+	    $id_logado = $linha['id'];
+	
+	    $consulta = $db_con->prepare("SELECT * FROM evento WHERE evento.id=$id AND evento.fk_usuario_id=$id_logado");
+	    if($consulta->execute()){
+		    $linha = $consulta->fetch(PDO::FETCH_ASSOC);
+		    if ($linha){
+			    return true;
+		    }
+		    return false;
+	    }
+    } else {
+	    return false;
     }
 }
 ?>
