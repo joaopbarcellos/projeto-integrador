4554d23aa380b7bfaebe92791244b59485a71c8b
Pedro Ramos Leite Soares <91470099+pedrorl11@users.noreply.github.com>
2023-12-11 15:32:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -355,7 +355,7 @@ session_start();
 							<svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
 								<path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/>
 							</svg>
-							Outros
+							Outro
 						</div>
 					</button>
 				</div>
